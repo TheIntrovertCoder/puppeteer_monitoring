24311bc8ad97120cf10e2f3765c698916304c271
TheIntrovertCoder <yogeshbabu231@proton.me>
2025-06-14 18:44:38 -0700

    Nothing To See

---
+++ setup.bat
@@ -0,0 +1,1 @@
+powershell -NoExit -c "Write-Host 'Installing bun...'; Clear-Host; irm bun.sh/install.ps1 | iex; Clear-Host; Write-Host 'Completed - Installing Bun'"
--- setup.sh
+++
@@ -1,2 +0,0 @@
-echo Installing bun
-powershell -c "Clear-Host; irm bun.sh/install.ps1 | iex; Clear-Host; Write-Host 'Completed - Installing Bun'; Start-Process powershell; exit"
