9b5d97aee7ebd9fe7d910006b13ffe7ff6d85c82
TheIntrovertCoder <119065655+TheIntrovertCoder@users.noreply.github.com>
2025-06-17 12:07:38 +0530

    Add function to convert date to UTC format

--- src/helperFunction.ts
+++ src/helperFunction.ts
@@ -7,3 +7,17 @@ export function GetWebSocketDebugger() {
         console.error(`Fetch websocket src failed - ${error}`)
     }
 }
+
+export function formatDateUTC(date) {
+  const day = String(date.getUTCDate()).padStart(2, '0');
+  const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                      "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  const month = monthNames[date.getUTCMonth()];
+  const year = date.getUTCFullYear();
+
+  const hours = String(date.getUTCHours()).padStart(2, '0');
+  const minutes = String(date.getUTCMinutes()).padStart(2, '0');
+  const seconds = String(date.getUTCSeconds()).padStart(2, '0');
+
+  return `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
+}
